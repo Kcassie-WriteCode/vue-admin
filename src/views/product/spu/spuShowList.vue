437885a68189bad50e53f254feec7c40fd6dcc8c
Kcassie-WriteCode <3125921734@qq.com>
2020-12-15 11:06:11 +0800

    feat:多图片上传问题解决

--- src/views/product/spu/spuShowList.vue
+++ src/views/product/spu/spuShowList.vue
@@ -1,7 +1,13 @@
 <template>
-  <el-card style="margin-top: 20px">
+  <el-card style="margin-top: 20px" v-loading="loading">
     <!-- :disabled="!category.category3Id" -->
-    <el-button type="primary" icon="el-icon-plus">添加SPU</el-button>
+    <el-button
+      type="primary"
+      icon="el-icon-plus"
+      :disabled="category.category3Id > 0 ? false : true"
+      @click="$bus.$emit('showUpdateList')"
+      >添加SPU</el-button
+    >
     <el-table :data="spuList" border style="width: 100%; margin: 20px 0">
       <!--  v-loading="loading" -->
       <el-table-column type="index" label="序号" width="80" align="center">
@@ -45,21 +51,24 @@ export default {
   name: "SpuShowList",
   data() {
     return {
-      spuList: [],
-      total: 0,
-      page: 1,
-      limit: 3,
+      spuList: [], //获取spu分类列表
+      total: 0, //总条数
+      page: 1, //当前页码
+      limit: 3, //每页显示的条数
       category: {
         //要改变category定义为响应式
         category1Id: "",
         category2Id: "",
         category3Id: "",
       },
+      loading: false,
     };
   },
   methods: {
     //获取spulist数据
     async getPageList(page, limit) {
+      //发送数据请求之前先加载
+      this.loading = true;
       const { category3Id } = this.category;
       const result = await this.$API.spu.getSpuPageList({
         page,
@@ -73,11 +82,13 @@ export default {
       } else {
         this.$message.error("失败");
       }
+      //请求之后，无论成功与否，都设置为false，加载完成
+      this.loading = false;
     },
     //这里因为要卸载所以必须封装函数
     //因为分页器的函数已经封装好了，只能传两个参
     handleSpuPageList(category) {
-      this.category = category; //点击三级分类红获取category
+      this.category = category; //点击三级分类获取category
       this.getPageList(this.page, this.limit);
     },
     //再次点击分类的时候要清空spushowlist页面
--- src/views/product/spu/spuUpdateList.vue
+++ src/views/product/spu/spuUpdateList.vue
@@ -19,23 +19,30 @@
         <el-input type="textarea" v-model="spu.description"></el-input>
       </el-form-item>
       <el-form-item label="SPU图片">
+        <!-- accept接收的文件类型 list-type图片列表类型为卡片 -->
         <el-upload
+          accept="image/*"
           list-type="picture-card"
-          :file-list="imageList"
+          :file-list="formatImageList"
           :action="`${$BASE_API}/admin/product/fileUpload`"
           :on-preview="handlePictureCardPreview"
           :on-remove="handleRemove"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
         >
           <i class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
         <span>只能上传jpg/png文件，且不超过50kb</span>
       </el-form-item>
-      <el-form-item label="销售属性">
-        <el-select v-model="spu.tmId" placeholder="请选择品牌">
+      <el-form-item label="销售属性" prop="saleAttrId">
+        <el-select
+          v-model="spu.saleAttrId"
+          :placeholder="`还有${filterSaleAttrList.length}未选择`"
+        >
           <el-option
             :label="saleAttr.name"
             :value="saleAttr.id"
-            v-for="saleAttr in saleAttrList"
+            v-for="saleAttr in filterSaleAttrList"
             :key="saleAttr.id"
           ></el-option>
         </el-select>
@@ -90,11 +97,63 @@ export default {
       spuSaleAttrList: [], //获取销售属性列表
     };
   },
+  computed: {
+    // 在所有的销售属性列表里面过滤掉
+    //已经选择在table里面的数据  单个spu销售属性 //留下未选择的
+    filterSaleAttrList() {
+      return this.saleAttrList.filter((saleAttr) => {
+        return !this.spuSaleAttrList.find(
+          (spuSaleAttr) => spuSaleAttr.baseSaleAttrId === saleAttr.id
+        );
+      });
+    },
+    //处理图片资源进行动态展示 /因为element ul中使用的字段和请求回来的字段名不一样
+    //但是后期还要上传图片，所以不能直接改data数据字段
+    formatImageList() {
+      return this.imageList.map((img) => {
+        return {
+          //解决element ui上传多图片时会闪现的bug
+          //设置一个uid，原数据img刚请求回来是没有uid的，所有得设置为img.id
+          uid: img.uid || img.id,
+          name: img.imgName,
+          url: img.imgUrl,
+        };
+      });
+    },
+  },
   methods: {
+    //上传图片成功回调函数 //往图片列表里面push新添加的图片
+    handleAvatarSuccess(res, file) {
+      //console.log(res.data); //图片地址
+      return this.imageList.push({
+        imgName: file.name,
+        imgUrl: res.data,
+        id: this.spu.id,
+        uid: file.uid,//解决多图片上传会闪现问题
+      });
+    },
+    //上传图片之前进行图片大小和格式校验
+    beforeAvatarUpload(file) {
+      //可以上传的图片类型
+      const imgTypes = ["image/jpeg", "image/png", "image/jpg"];
+      // 检测文件类型条件 // file.type为上传的图片类型
+      const isValidType = imgTypes.indexOf(file.type) > -1;
+      //图片大小条件
+      const isLt = file.size / 1024 < 50;
+
+      if (!isValidType) {
+        this.$message.error("上传头像图片只能是 jpg/png格式!");
+      }
+      if (!isLt) {
+        this.$message.error("上传头像图片大小不能超过50kb!");
+      }
+      return isValidType && isLt;
+    },
     // 处理删除
     handleRemove(file, fileList) {
-      // console.log(file, fileList);
-      this.imageList = this.imageList.filter((img) => img.id !== file.id);
+      //图片id的话，点击删除会删除全部
+      //用地址唯一性
+      this.imageList = this.imageList.filter((img) => img.imgUrl !== file.url);
     },
     // 处理图片预览
     handlePictureCardPreview(file) {
@@ -118,13 +177,7 @@ export default {
       const result = await this.$API.spu.getSpuImageList(id);
       if (result.code === 200) {
         this.$message.success("图片列表获取成功");
-        this.imageList = result.data.map((img) => {
-          return {
-            name: img.imgName,
-            url: img.imgUrl,
-            id: img.id,
-          };
-        });
+        this.imageList = result.data;
       } else {
         this.$message.error(error.message);
       }
@@ -139,11 +192,10 @@ export default {
         this.$message.error(error.message);
       }
     },
-    //获取属性列表
+    //获取单个spu销售属性
     async getsSpuSaleAttrList() {
       const { id } = this.spu;
       const result = await this.$API.spu.getsSpuSaleAttrList(id);
-      console.log(result);
       if (result.code === 200) {
         this.$message.success("品牌列表获取成功");
         this.spuSaleAttrList = result.data;
